94c5b13db5de0df57215176c8febd192e26ecaea
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-09 00:16:47 +0530

    Update MirrorNumTriangle.java

--- MirrorNumTriangle.java
+++ MirrorNumTriangle.java
@@ -1,7 +1,6 @@
 public class MirrorNumTriangle {
     public static void main(String[] args) {
         int n = 5;
-
         for(int i = 1; i <= n; ++i) {
             int l;
             for(l = i; l < n; ++l) {
