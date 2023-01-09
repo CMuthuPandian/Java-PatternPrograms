0b6ed8963f82d3541afcaf78922cee630a499575
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-10 00:15:17 +0530

    Update NumRevPattern.java

--- NumRevPattern.java
+++ NumRevPattern.java
@@ -2,16 +2,13 @@ public class NumRevPattern {
     public static void main(String[] args) {
         int n = 6;
         int p = n;
-
         for(int i = 1; i <= n; ++i) {
             for(int j = p; j >= 1; --j) {
                 System.out.print("" + i + " ");
             }
-
             --p;
             System.out.println();
          }
-
     }
 }
 
