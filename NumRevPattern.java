8e18cbddfef566027532167e9757019c9641b780
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-10 00:13:00 +0530

    Update NumRevPattern.java

--- NumRevPattern.java
+++ NumRevPattern.java
@@ -10,7 +10,20 @@ public class NumRevPattern {
 
             --p;
             System.out.println();
-            }
+         }
 
-        }
+    }
 }
+
+/*
+
+Output:
+
+1 1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 
+4 4 4 
+5 5 
+6
+
+ */
