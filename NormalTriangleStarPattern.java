03e581697ce6880c61f2df35b50ace0e12d7bcd2
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-10 00:09:52 +0530

    Add files via upload

---
+++ NormalTriangleStarPattern.java
@@ -0,0 +1,21 @@
+public class NormalTriangleStarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        int column = 1;
+
+        for(int i = 1; i <= n; ++i) {
+            int k;
+            for(k = i; k < n; ++k) {
+                System.out.print(" ");
+            }
+
+            for(k = 1; k <= column; ++k) {
+                System.out.print("*");
+            }
+
+            column += 2;
+            System.out.println();
+        }
+
+    }
+}
