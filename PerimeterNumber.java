54cd84e52cbaa6c10de18caa4af782771bfa7f6b
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-11 01:10:46 +0530

    Add files via upload

---
+++ PerimeterNumber.java
@@ -0,0 +1,24 @@
+public class PerimeterNumber {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = 1; i < 2 * n; ++i) {
+            for(int j = 1; j < 2 * n; ++j) {
+                if (i != 1 && i != 7 && j != 1 && j != 7) {
+                    if (i == n && j == n) {
+                        System.out.print("1 ");
+                    } else if (i != 3 && i != 4 && i != 5 || j != 3 && j != 4 && j != 5) {
+                        System.out.print("3 ");
+                    } else {
+                        System.out.print("2 ");
+                    }
+                } else {
+                    System.out.print("4 ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
