2f748f6d78890e47cb33a9216b39a014e4b47e83
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-12 18:00:33 +0530

    Add files via upload

---
+++ RhombusNumber.java
@@ -0,0 +1,41 @@
+public class RhombusNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 1;
+        int space = 1;
+
+        for(int i = 1; i < n * 2; ++i) {
+            int j;
+            for(j = n - space; j > 0; --j) {
+                System.out.print(" ");
+            }
+
+            int start;
+            if (i < n) {
+                start = i;
+                ++space;
+            } else {
+                start = n * 2 - i;
+                --space;
+            }
+
+            for(j = 0; j < count; ++j) {
+                System.out.print(start);
+                if (j < count / 2) {
+                    --start;
+                } else {
+                    ++start;
+                }
+            }
+
+            if (i < n) {
+                count += 2;
+            } else {
+                count -= 2;
+            }
+
+            System.out.println();
+        }
+
+    }
+}
