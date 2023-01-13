bd88776b964cf67a1b921e6d353804e9b59cef17
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-13 18:16:25 +0530

    Add files via upload

---
+++ TrapeziumPattern.java
@@ -0,0 +1,35 @@
+public class TrapeziumPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        int first = 1;
+        int second = n * n + 1;
+        int space = 0;
+
+        for(int i = n; i >= 1; --i) {
+            int l;
+            for(l = space; l >= 1; --l) {
+                System.out.print(" ");
+            }
+
+            int var10001;
+            for(l = 1; l <= i; ++l) {
+                var10001 = first++;
+                System.out.print("" + var10001 + " ");
+            }
+
+            for(l = 1; l <= i; ++l) {
+                if (first == second) {
+                    System.out.print(second++);
+                } else {
+                    var10001 = second++;
+                    System.out.print(" " + var10001);
+                }
+            }
+
+            System.out.println();
+            space += 2;
+            second = second - 1 - (i - 1) * 2;
+        }
+
+    }
+}
