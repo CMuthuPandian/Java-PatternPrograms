2d391b3b02ca168a16e8c133585037072207a845
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-13 00:57:15 +0530

    Update OddEvenPattern.java

--- OddEvenPattern.java
+++ OddEvenPattern.java
@@ -38,7 +38,6 @@ Sample Output 1
  */
 import java.util.Scanner;
 public class OddEvenPattern {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
