68260337744af68e69e35c79375ef369c5ce2f56
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-12 14:58:25 +0530

    Add files via upload

---
+++ InvertedTriangle2n.java
@@ -0,0 +1,48 @@
+/*
+
+To print inverted pyramid star pattern
+
+Input Format
+Int -> Integer
+
+Constraints
+No Constraints
+
+Output Format
+Print the pattern
+
+Sample Input 0
+5
+
+Sample Output 0
+
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+ */
+
+import java.util.Scanner;
+
+public class InvertedTriangle2n {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int count = 2*n;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<count;k++){
+                System.out.print("* ");
+            }
+            count=count-2;
+            System.out.println();
+        }
+
+    }
+
+}
