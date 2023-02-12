b1f9812ba99de0d45469adf2ad15c643c7c8f46f
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-12 15:00:17 +0530

    Update InvertedTriangle2n.java

--- InvertedTriangle2n.java
+++ InvertedTriangle2n.java
@@ -25,7 +25,6 @@ Sample Output 0
  */
 import java.util.Scanner;
 public class InvertedTriangle2n {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
