e40304ded6781f4883de2eb2aa2349725e5d1db6
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-13 20:04:57 +0530

    Update XStarPattern.java

--- XStarPattern.java
+++ XStarPattern.java
@@ -42,7 +42,6 @@ Sample Output 1
  */
 import java.util.Scanner;
 public class XStarPattern {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
