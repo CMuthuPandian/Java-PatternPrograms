f06e23118a77cc637195157b52035194801087b9
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-14 21:43:32 +0530

    Update LeftArrowStarPattern.java

--- LeftArrowStarPattern.java
+++ LeftArrowStarPattern.java
@@ -27,11 +27,8 @@ Sample Output 0
     *****
 
  */
-
 import java.util.Scanner;
-
 public class LeftArrowStarPattern {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
